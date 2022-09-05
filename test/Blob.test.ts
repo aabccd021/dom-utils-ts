9ee89fed2a75747e96765a4685348a75ddfcf787
aabccd021 <aabccd021@gmail.com>
2022-09-06 06:13:04 +0700

    refactor: upgrade unit-test-ts

--- test/Blob.test.ts
+++ test/Blob.test.ts
@@ -1,18 +1,18 @@
 import { pipe } from 'fp-ts/function';
-import { it, itFail, runTests, Tests } from 'unit-test-ts';
+import { executeTests, itExpect, itExpectFail, Tests } from 'unit-test-ts';
 import * as vitest from 'vitest';
 
 import * as blob from '../src/Blob';
 
 const tests: Tests = {
-  'fromString then text returns original string': it({
-    expectTask: pipe('masumoto', blob.fromString, blob.text),
+  'fromString then text returns original string': itExpect({
+    task: pipe('masumoto', blob.fromString, blob.text),
     toEqual: 'masumoto',
   }),
-  'fromString then text does not return another string': itFail({
-    expectTask: pipe('masumoto', blob.fromString, blob.text),
+  'fromString then text does not return another string': itExpectFail({
+    task: pipe('masumoto', blob.fromString, blob.text),
     toEqual: 'nazuna',
   }),
 };
 
-runTests(tests, vitest);
+executeTests(tests, vitest);
